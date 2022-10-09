6fe974f28b7764c5f3266ae2f92072da8c1fdaf6
DRABARAN <cfymjr@gmail.com>
2022-10-09 12:19:28 +0200

    fix: target info incorrect size

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -45,42 +45,42 @@ type BuffBarInfo struct {
 
 // Extended info about player's character
 type ExtendedInfo struct {
-	MaxWeight          uint16 `bin:"len:4"`
-	Race               byte   `bin:"len:1"`
-	StatCap            uint16 `bin:"len:4"`
-	PetsCurrent        byte   `bin:"len:1"`
-	PetsMax            byte   `bin:"len:1"`
-	FireResist         uint16 `bin:"len:4"`
-	ColdResist         uint16 `bin:"len:4"`
-	PoisonResist       uint16 `bin:"len:4"`
-	EnergyResist       uint16 `bin:"len:4"`
-	Luck               int16  `bin:"len:4"`
-	DamageMin          uint16 `bin:"len:4"`
-	DamageMax          uint16 `bin:"len:4"`
-	TithingPoints      uint32 `bin:"len:8"`
-	HitChanceIncr      uint16 `bin:"len:4"`
-	SwingSpeedIncr     uint16 `bin:"len:4"`
-	DamageChanceIncr   uint16 `bin:"len:4"`
-	LowerReagentCost   uint16 `bin:"len:4"`
-	HpRegen            uint16 `bin:"len:4"`
-	StamRegen          uint16 `bin:"len:4"`
-	ManaRegen          uint16 `bin:"len:4"`
-	ReflectPhysDamage  uint16 `bin:"len:4"`
-	EnhancePotions     uint16 `bin:"len:4"`
-	DefenseChanceIncr  uint16 `bin:"len:4"`
-	SpellDamageIncr    uint16 `bin:"len:4"`
-	FasterCastRecovery uint16 `bin:"len:4"`
-	FasterCasting      uint16 `bin:"len:4"`
-	LowerManaCost      uint16 `bin:"len:4"`
-	StrengthIncr       uint16 `bin:"len:4"`
-	DextIncr           uint16 `bin:"len:4"`
-	IntIncr            uint16 `bin:"len:4"`
-	HpIncr             uint16 `bin:"len:4"`
-	StamIncr           uint16 `bin:"len:4"`
-	ManaIncr           uint16 `bin:"len:4"`
-	MaxHpIncr          uint16 `bin:"len:4"`
-	MaxStamIncr        uint16 `bin:"len:4"`
-	MaxManaIncrease    uint16 `bin:"len:4"`
+	MaxWeight          uint16 `bin:"le"`
+	Race               byte   `bin:"le"`
+	StatCap            uint16 `bin:"le"`
+	PetsCurrent        byte   `bin:"le"`
+	PetsMax            byte   `bin:"le"`
+	FireResist         uint16 `bin:"le"`
+	ColdResist         uint16 `bin:"le"`
+	PoisonResist       uint16 `bin:"le"`
+	EnergyResist       uint16 `bin:"le"`
+	Luck               int16  `bin:"le"`
+	DamageMin          uint16 `bin:"le"`
+	DamageMax          uint16 `bin:"le"`
+	TithingPoints      uint32 `bin:"le"`
+	HitChanceIncr      uint16 `bin:"le"`
+	SwingSpeedIncr     uint16 `bin:"le"`
+	DamageChanceIncr   uint16 `bin:"le"`
+	LowerReagentCost   uint16 `bin:"le"`
+	HpRegen            uint16 `bin:"le"`
+	StamRegen          uint16 `bin:"le"`
+	ManaRegen          uint16 `bin:"le"`
+	ReflectPhysDamage  uint16 `bin:"le"`
+	EnhancePotions     uint16 `bin:"le"`
+	DefenseChanceIncr  uint16 `bin:"le"`
+	SpellDamageIncr    uint16 `bin:"le"`
+	FasterCastRecovery uint16 `bin:"le"`
+	FasterCasting      uint16 `bin:"le"`
+	LowerManaCost      uint16 `bin:"le"`
+	StrengthIncr       uint16 `bin:"le"`
+	DextIncr           uint16 `bin:"le"`
+	IntIncr            uint16 `bin:"le"`
+	HpIncr             uint16 `bin:"le"`
+	StamIncr           uint16 `bin:"le"`
+	ManaIncr           uint16 `bin:"le"`
+	MaxHpIncr          uint16 `bin:"le"`
+	MaxStamIncr        uint16 `bin:"le"`
+	MaxManaIncrease    uint16 `bin:"le"`
 }
 
 // Represents X or Y coordinate in UO world
@@ -99,9 +99,9 @@ type FoundTile struct {
 }
 
 type TargetInfo struct {
-	ID   uint32     `bin:"len:8"`
-	Tile uint16     `bin:"len:4"`
-	X    Coordinate `bin:"len:4"`
-	Y    Coordinate `bin:"len:4"`
-	Z    int8       `bin:"len:2"`
+	ID   uint32     `bin:"le"`
+	Tile uint16     `bin:"le"`
+	X    Coordinate `bin:"le"`
+	Y    Coordinate `bin:"le"`
+	Z    int8       `bin:"le"`
 }
